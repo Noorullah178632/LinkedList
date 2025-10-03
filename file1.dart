f4d602ed799e5bddad67a1295afd73cc04d551df
NoorUllah <nooullah178632@gmail.com>
2025-10-03 23:30:14 +0500

    data2

--- file1.dart
+++ file1.dart
@@ -1,11 +1,60 @@
 //class for node
-class Node {}
+class Node {
+  int data;
+  Node? prev;
+  Node? next;
+  Node(this.data);
+}
 
 //class for double linked list
-class DoubleLinkedList {}
+class DoubleLinkedList {
+  Node? head;
+  //insert at begin
+  void insertAtBegin(int data) {
+    Node newNode = Node(data);
+    if (head == null) {
+      head = newNode;
+      return;
+    }
+    newNode.next = head;
+    head!.prev = newNode;
+    head = newNode;
+  }
+
+  //add data at the end of the linked list
+  void addAtLast(int data) {
+    Node newNode = Node(data);
+    if (head == null) {
+      head = newNode;
+      return;
+    }
+    Node? temp = head!;
+    while (temp!.next != null) {
+      temp = temp.next;
+    }
+    temp.next = newNode;
+    newNode.prev = temp;
+  }
+
+  //show the whole linkedList
+  List<int> showLinkedList() {
+    Node? temp = head!;
+    List<int> storedData = [];
+    while (temp != null) {
+      storedData.add(temp.data);
+      temp = temp.next;
+    }
+    return storedData;
+  }
+}
 
 void main() {
-  Node a = Node();
+  DoubleLinkedList a = DoubleLinkedList();
+  a.insertAtBegin(20);
+  a.insertAtBegin(30);
+  a.addAtLast(40);
+  a.addAtLast(50);
+  print(a.showLinkedList());
 }
 //count=1 < 9 → temp=20, count=2
 
